052279d14d62938e9964c88984f8720f04a85916
elppaaa <withbcd@gmail.com>
2021-07-20 00:08:17 +0900

    Initial Commit

---
+++ MyPhotos/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyPhotos
+//
+//  Created by JK on 2021/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.backgroundColor = .systemBackground
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

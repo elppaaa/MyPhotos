3cba4efe1bd4b9833341f3ee9060cea8994b0355
elppaaa <withbcd@gmail.com>
2021-07-20 14:29:40 +0900

    [feat] Add photo acess on AlbumListVC

--- MyPhotos/AlbumCell.swift
+++ MyPhotos/AlbumCell.swift
@@ -7,4 +7,20 @@
 
 import UIKit
 
-final class AlbumCell: UITableViewCell { }
+final class AlbumCell: UITableViewCell {
+  override var reuseIdentifier: String? { Self.identifier }
+  required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+  init() {
+    super.init(style: .default, reuseIdentifier: Self.identifier)
+
+    configLayout()
+  }
+
+  private(set) lazy var thumbnail = UIImageView()
+  private(set) lazy var title = UILabel()
+  private(set) lazy var photosCount = UILabel()
+}
+
+extension AlbumCell {
+  private func configLayout() { }
+}
--- MyPhotos/AlbumListViewController.swift
+++ MyPhotos/AlbumListViewController.swift
@@ -6,5 +6,53 @@
 //
 
 import UIKit
+import Photos
+import RxSwift
 
-final class AlbumListViewController: UITableViewController { }
+final class AlbumListViewController: UITableViewController {
+  var allPhotos: PHFetchResult<PHAsset>?
+  let disposeBag = DisposeBag()
+
+  override func loadView() {
+    super.loadView()
+    view.backgroundColor = .systemBackground
+    configTableView()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    PhotoLibararyManager.requstAuthorization()
+      .observe(on: MainScheduler.instance)
+      // tableView reload when complte
+      .subscribe(onCompleted: {  },
+                 onError: alertPhotoAccessDenied)
+      .disposed(by: disposeBag)
+  }
+}
+
+extension AlbumListViewController {
+  /// 사진 접근 권한 실패 시 alert
+  private func alertPhotoAccessDenied(_ err: Error) {
+    let vc = UIAlertController(title: "권한 획득 실패",
+                               message: "사진 접근에 실패하였습니다.\n모든 사진에 대한 권한을 허용해주세요.",
+                               preferredStyle: .alert)
+    let OKAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+    let goSettingsAction = UIAlertAction(title: "설정으로 이동", style: .default) { _ in
+      guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+      UIApplication.shared.open(url)
+    }
+
+    vc.addAction(goSettingsAction)
+    vc.addAction(OKAction)
+
+    present(vc, animated: true, completion: nil)
+  }
+}
+
+extension AlbumListViewController {
+  private func configTableView() {
+    tableView.register(AlbumCell.self, forCellReuseIdentifier: AlbumCell.identifier)
+    tableView.rowHeight = 85.0
+  }
+}
